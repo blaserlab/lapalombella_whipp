defba03bb0e46c0cd158308781ccd5619b5f6030
blaserlab <bradley.blaser@osumc.edu>
2022-03-09 09:54:38 -0500

    added blaseRtemplates

--- R/configs.R
+++ R/configs.R
@@ -1,1 +1,17 @@
 theme_set(theme_cowplot(font_size = 11))
+
+# conflicts ---------------------------------------------------------------
+# resolve conflicting function names here
+
+conflict_prefer("filter", "dplyr")
+conflict_prefer("lag", "dplyr")
+conflict_prefer("select", "dplyr")
+conflict_prefer("rename", "dplyr")
+conflict_prefer("count", "dplyr")
+
+# source local configs ----------------------------------------------------
+# these are sourced after main configs and will overwrite duplicate entries if
+# present. The file local_configs.R is ignored by git and so is useful for user-
+# specific configurations such as output directories or formatting.
+
+source("R/local_configs.R")
